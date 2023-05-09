00a1156b27754242b57e68030dea69a0d7afd27e
douglascastrorj <douglascastrorj@gmail.com>
2023-05-09 23:49:08 +0000

    criando form na tela inicial

--- src/context/riotapi.ts
+++ src/context/riotapi.ts
@@ -1,10 +1,16 @@
 const api_summoner_v4 = 'https://br1.api.riotgames.com/lol/summoner/v4';
 const api_league_v4 = 'https://br1.api.riotgames.com/lol/league/v4';
 
-export function getSummonerByName(api_key: string, summoner_name: string) {
-    return fetch(`${api_summoner_v4}/summoners/by-name/${summoner_name}?api_key=${api_key}`);
+const api_key = 'RGAPI-82fb1f77-0e22-44ed-8f71-3b1640780c17';
+
+export function getSummonerByName(summoner_name: string) {
+  return fetch(`${api_summoner_v4}/summoners/by-name/${summoner_name}?api_key=${api_key}`).then((response) => {
+    return response.json();
+  });
 }
 
-export function getEntriesBySummoner(api_key: string, encrypted_summoner_id: string) {
-    return fetch(`${api_league_v4}/entries/by-summoner/${encrypted_summoner_id}?api_key=${api_key}`);
+export function getEntriesBySummoner(encrypted_summoner_id: string) {
+  return fetch(`${api_league_v4}/entries/by-summoner/${encrypted_summoner_id}?api_key=${api_key}`).then((response) => {
+    return response.json();
+  });
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './Home.css'
 import { getSummonerByName } from '../../context/riotapi.ts';
+import { Button, TextField } from '@material-ui/core';
 
 export default function Home() {
 
+  const [name, setName] = useState('');
+  const [summoner, setSummoner] = useState({});
+
   useEffect(() => {
     document.title = 'Liga das Lendas React ⚛️';
-
-    fetchSummonerByName('Merkurio Sensei');
   }, []);
 
-  const fetchSummonerByName = async (name) => {
-    getSummonerByName('RGAPI-82fb1f77-0e22-44ed-8f71-3b1640780c17', name).then((response) => {
+  const fetchSummonerByName = async (name: string) => {
+    getSummonerByName(name).then((response: any) => {
       console.log(response);
+      setSummoner(response);
     });
+  }
+
+  const handleChange = (event: any) => {
+    setName(event.target.value);
+  }
+
+  const handleSubmit = (event: any) => {
+    fetchSummonerByName(name);
+    event.preventDefault();
   }
 
   return (
     <main>
-      Liga das Lendas React ⚛️
+      <span class="title"> Liga das Lendas React </span>
+
+      <div class="form">
+        {/* <input class="search" type="text" onChange={handleChange} /> */}
+        <TextField id="standard-basic" variant="outlined"  onChange={handleChange} />
+        <Button variant="contained" color="primary" onClick={handleSubmit}> Buscar </Button>
+      </div>
+
+        { summoner.id ? 
+        (
+          <div>
+            <pre>{JSON.stringify(summoner, null, 2) }</pre>
+          </div>
+        ) : null
+      }
+      
     </main>
   )
 }
