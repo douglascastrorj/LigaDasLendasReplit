d389d802482787a138d40afb2feb9016d2f45dc4
ronnimartins <84156743+ronnimartins@users.noreply.github.com>
2023-05-13 18:30:58 +0000

    lendo dados da api

--- src/context/riotapi.ts
+++ src/context/riotapi.ts
@@ -1,7 +1,8 @@
 const api_summoner_v4 = 'https://br1.api.riotgames.com/lol/summoner/v4';
 const api_league_v4 = 'https://br1.api.riotgames.com/lol/league/v4';
 
-const api_key = 'RGAPI-82fb1f77-0e22-44ed-8f71-3b1640780c17';
+
+const api_key = 'RGAPI-d037c10a-5d55-4ae6-9e6a-4d588a46fea8';
 
 export function getSummonerByName(summoner_name: string) {
   return fetch(`${api_summoner_v4}/summoners/by-name/${summoner_name}?api_key=${api_key}`).then((response) => {
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './Home.css'
-import { getSummonerByName } from '../../context/riotapi.ts';
+import { getSummonerByName, getEntriesBySummoner } from '../../context/riotapi.ts';
 import { Button, TextField } from '@material-ui/core';
 //https://v4.mui.com/components/popover/
 
@@ -8,15 +8,23 @@ export default function Home() {
 
   const [name, setName] = useState('');
   const [summoner, setSummoner] = useState({});
+  const [entries, setEntries] = useState([]);
 
   useEffect(() => {
     document.title = 'Liga das Lendas React ⚛️';
   }, []);
 
-  const fetchSummonerByName = async (name: string) => {
-    getSummonerByName(name).then((response: any) => {
-      console.log(response);
-      setSummoner(response);
+  const fetchSummonerByName = async (summonerName: string) => {
+    getSummonerByName(summonerName).then((summoner: any) => {
+      setSummoner(summoner);
+      fetchEntriesBySummonerId(summoner.id)
+    });
+  }
+
+  const fetchEntriesBySummonerId = async (id: string) => {
+    getEntriesBySummoner(id).then((response: any) => {
+      console.log('Entries: ',response);
+      setEntries(response);
     });
   }
 
@@ -29,25 +37,58 @@ export default function Home() {
     event.preventDefault();
   }
 
+
+  const renderEntry = (entry) => {
+    if(!entry) return null;
+    let winRate = entry.wins / (entry.wins + entry.losses)
+    winRate = winRate * 100
+
+    let queueType = 'Ranqueada Solo';
+    if(entry.queueType == 'RANKED_FLEX_SR') queueType = 'Ranqueada Flexível';
+
+    const eloImage = `src/assets/images/icons-elo/${entry.tier.toLowerCase()}.png`
+    return (
+      <>
+        <div>
+          <span>
+            {entry.tier} {entry.rank} - 
+            {entry.leaguePoints} LP
+            {queueType}
+            Win {entry.wins}
+            lose {entry.losses}
+            winrate {Math.round(winRate)}%
+          </span>
+          <div class="elo">
+            <img width="80px" src={eloImage} />
+          </div>
+        </div>
+      </>
+    )
+  }
+
   return (
     <main>
       <span class="title"> Liga das Lendas React </span>
 
       <div class="form">
         {/* <input class="search" type="text" onChange={handleChange} /> */}
-        <TextField id="standard-basic" variant="outlined"  onChange={handleChange} />
+        <TextField id="standard-basic" variant="outlined" onChange={handleChange} />
         <Button variant="contained" color="primary" onClick={handleSubmit}> Buscar </Button>
       </div>
 
-        { summoner.id ? 
+      {/* {summoner.id ?
         (
           <div>
-            <pre>{JSON.stringify(summoner, null, 2) }</pre>
+            <pre>{JSON.stringify(summoner, null, 2)}</pre>
             <span class="vermelho"> {summoner.name} </span>
           </div>
         ) : null
+      } */}
+
+      {
+        entries.map(entry => renderEntry(entry))
       }
-      
+
     </main>
   )
 }
